57e883084c27985f52a4c424726e1d1c00e7fa60
M1ndy <gdm1ndy234@gmail.com>
2026-01-29 23:51:41 -0500

    Fix typo in Phrase_2 variable name and improve input prompts for consistency

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Phrase_1 = ["You're written in every beat of my heart",
             "I'm lost in everything you are",
             "You're the dream I never want to wake from"]
 
-Pharse_2 = ["With you, I've found my forever",
+Phrase_2 = ["With you, I've found my forever",
             "You're the missing piece I didn't know I needed",
             "Every part of me belongs to you",
             "You're my once in a lifetime",
@@ -15,8 +15,9 @@ Pharse_2 = ["With you, I've found my forever",
 
 def lyric_mix():
     while True:
+        # Reuse the copy(), helps save us from errors that may pop up with removing from lists while looping within them.
         list_1 = Phrase_1.copy()
-        list_2 = Pharse_2.copy()
+        list_2 = Phrase_2.copy()
             
         for i in range(1, 6):
             chosen_phrase_1 = random.choice(list_1)
@@ -27,7 +28,8 @@ def lyric_mix():
 
             print(f"{str(i)}. {chosen_phrase_1}, {chosen_phrase_2}")
 
-    
+        # There defininetly is a better way to end this, but this works for now.
+        # Currently, allows for endless regenaration, which could be a feature so not too urgent to correct.
         confirm = input("Would you like to generate 5 more? (yes/no): ").strip().lower()
 
         if confirm.startswith("n"):
@@ -43,7 +45,7 @@ print("While it ain't much, we get by.")
 print("So hop on in and generate your five romanctic lyrics.")
 print("If you do it just right, and have some game to your name, you'll be alright.")
 
-while option := input("Generate or no?: ").strip().capitalize():
+while option := input("Generate or No?: ").strip().capitalize():
     match option:
         case "Generate":
             lyric_mix()
